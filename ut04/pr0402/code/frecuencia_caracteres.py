e57f4aed62154eddcc580b4a93e6609a53e7acf2
kinire98 <iknitgarri@outlook.com>
2024-10-29 12:23:29 +0100

    añadidos ejercicios nuevos

---
+++ ut04/pr0402/code/frecuencia_caracteres.py
@@ -0,0 +1,7 @@
+def frecuencia_caracteres(string: str) -> dict:
+    dictionary = {}
+    for c in string:
+        dictionary[c] = dictionary.get(c, 0) + 1
+    return dictionary
+
+print(frecuencia_caracteres(input("Introduce una cadena para saber la frecuencia de caracteres: ")))
